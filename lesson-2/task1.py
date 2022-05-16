cc67ce73e7ff6eecb077bdd21b1f617d29a556c6
owertone.learning <owertone.learning@gmail.com>
2022-05-16 21:33:58 +0300

    Create task1.py
    Чуть не пропустил

---
+++ lesson-2/task1.py
@@ -0,0 +1,15 @@
+types_list = [1,
+              3.14,
+              1 + 1j,
+              'str',
+              [2, 'text'],
+              (3, 33),
+              {1, 2.3, 'dtr', False, (1, 2)},
+              False,
+              b'text',
+              None]
+
+for i in types_list:
+    print(type(i))
+
+# как вывести тип данных исключение?
