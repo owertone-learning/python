5b7eebff0335bb311f19cde61a33305c6deffb7e
owertone.learning <owertone.learning@gmail.com>
2022-05-21 23:57:46 +0300

    Create task 1
    Task 1, lesson 5

--- lesson-5/task2.py
+++
@@ -1,9 +0,0 @@
-with open('task1.txt') as f:
-    content = f.readlines()
-    number = 0
-    if content[-1] == '\n':
-        content = content[:-1]
-    print(f'Число строк: {len(content)}')
-    for line in content:
-        number += 1
-        print(f'Число слов в стороке {number}: {len(line.split())}')
